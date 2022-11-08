7e0ca56e14ad52a931ae77deb26dc8bba92c498f
zuble <67658465+zuble@users.noreply.github.com>
2022-11-08 19:15:10 +0000

    upup

---
+++ dog-cat/nia_mio.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+from keras import layers
+
+inputs = keras.Input(shape=(180, 180, 3))
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.summary()
+
+model.compile(loss="binary_crossentropy",
+            optimizer="rmsprop",
+            metrics=["accuracy"])

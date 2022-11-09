1dc1b18da9fd4158f3082beeff049ce1746e8408
zuble <67658465+zuble@users.noreply.github.com>
2022-11-09 18:26:20 +0000

    up

--- dog-cat/nia_mio.py
+++ dog-cat/nia_mio.py
@@ -1,8 +1,168 @@
+import os, shutil, pathlib
+import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from keras import layers, models
+from keras.utils import image_dataset_from_directory
 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+orig_dir = pathlib.Path("dogs-vs-cats/train")
+new_base_dir = pathlib.Path("dogs-vs-cats-small")
+
+def make_subset(subset_name, start_index, end_index):
+    for category in ("cat", "dog"):
+        dir = new_base_dir / subset_name / category
+        os.makedirs(dir)
+        fnames = [f"{category}.{i}.jpg" for i in range(start_index, end_index)]
+        for fname in fnames:
+            shutil.copyfile(src=orig_dir / fname,
+                            dst=dir / fname)
+
+#make_subset("train", start_index=0, end_index=1000)
+#make_subset("validation", start_index=1000, end_index=1500)
+#make_subset("test", start_index=1500, end_index=2500)
+
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size=(180, 180),
+    batch_size=32)
+
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size=(180, 180),
+    batch_size=32)
+
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size=(180, 180),
+    batch_size=32)
+
+for data_batch, labels_batch in train_dataset:
+    print("data batch shape:", data_batch.shape)
+    print("labels batch shape:", labels_batch.shape)
+    break
+
+
+
+'''
+WITHOUT DATA AUGMENTATION
+'''
+def form_model():
+    inputs = keras.Input(shape=(180, 180, 3))
+    x = layers.Rescaling(1./255)(inputs)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.Flatten()(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    model.summary()
+
+    model.compile(loss="binary_crossentropy",
+                optimizer="rmsprop",
+                metrics=["accuracy"])
+    return model
+
+def train_model(model):
+    callbacks = [keras.callbacks.ModelCheckpoint(
+                    filepath="convnet_from_scratch.keras",
+                    save_best_only=True,
+                    monitor="val_loss")]
+    history = model.fit(
+                train_dataset,
+                epochs=30,
+                validation_data=validation_dataset,
+                callbacks=callbacks)
+    return history
+
+def plt_model(history):
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    epochs = range(1, len(accuracy) + 1)
+    plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+    plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+    plt.title("Training and validation accuracy")
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, "bo", label="Training loss")
+    plt.plot(epochs, val_loss, "b", label="Validation loss")
+    plt.title("Training and validation loss")
+    plt.legend()
+    plt.show()
+
+def test_model():
+    test_model = keras.models.load_model("convnet_from_scratch.keras")
+    test_loss, test_acc = test_model.evaluate(test_dataset)
+    print(f"Test accuracy wo/ augm: {test_acc:.3f}")
+
+
+model = form_model()
+#history = train_model(model)
+#plt_model(history)
+#test_model()
+
+
+
+img_path = '/media/jtstudents/HDD/.zuble/vigia/dog-cat/cat.1700.jpg'
+img = tf.keras.utils.load_img(img_path, target_size=(180,180))
+img_tensor = tf.keras.preprocessing.image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
+plt.imshow(img_tensor[0])
+#plt.show()
+
+
+
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+activations = activation_model.predict(img_tensor)
+
+frist_layer = activations[0]
+print("\nfrist layer",frist_layer.shape)
+plt.matshow(frist_layer[0, :, :, 2], cmap='viridis')
+plt.show()
+
+
+
+'''
+
+"""
+WITH DATA AUGMENTATION
+"""
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+plt.figure(figsize=(10, 10))
+for images, _ in train_dataset.take(1):
+    for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[0].numpy().astype("uint8"))
+        plt.axis("off")
+
+        
 inputs = keras.Input(shape=(180, 180, 3))
-x = layers.Rescaling(1./255)(inputs)
+x = data_augmentation(inputs)
+x = layers.Rescaling(1./255)(x)
 x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
 x = layers.MaxPooling2D(pool_size=2)(x)
 x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
@@ -13,11 +173,29 @@ x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
 x = layers.MaxPooling2D(pool_size=2)(x)
 x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
 x = layers.Flatten()(x)
+x = layers.Dropout(0.5)(x)
 outputs = layers.Dense(1, activation="sigmoid")(x)
 model = keras.Model(inputs=inputs, outputs=outputs)
 
-model.summary()
-
 model.compile(loss="binary_crossentropy",
-            optimizer="rmsprop",
-            metrics=["accuracy"])
+                optimizer="rmsprop",
+                metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath="convnet_from_scratch_with_augmentation.keras",
+        save_best_only=True,
+        monitor="val_loss")
+]
+history = model.fit(
+    train_dataset,
+    epochs=100,
+    validation_data=validation_dataset,
+    callbacks=callbacks)
+
+test_model = keras.models.load_model(
+    "convnet_from_scratch_with_augmentation.keras")
+test_loss, test_acc = test_model.evaluate(test_dataset)
+print(f"Test accuracy w/ augm: {test_acc:.3f}")
+
+'''

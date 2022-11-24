421aa4e3a43200810ccdd940b3397234fe519373
zuble <67658465+zuble@users.noreply.github.com>
2022-11-24 10:45:32 +0000

    up

--- zhen_helperino/anomaly_zu.py
+++ zhen_helperino/anomaly_zu.py
@@ -11,11 +11,21 @@ import numpy as np
 import keras
 from keras import models, layers
 import tensorflow as tf
-import os, time, random, time
+import os, time, random, logging
 from tqdm.keras import TqdmCallback
 import pandas as pd
 
-#frame_no = 8
+def set_tf_loglevel(level):
+    if level >= logging.FATAL:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    if level >= logging.ERROR:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    if level >= logging.WARNING:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+    else:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+    logging.getLogger('tensorflow').setLevel(level)
+
 
 def input_video_data(file_name):
     print("\n\nINPUT_VIDEO_DATA\n")
@@ -157,11 +167,13 @@ def generate_input():
             yield batch_frames,  np.array([np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,1])]) 
         '''
         
+        #batch_frames
         if 'label_A' in train_fn[index]:
             yield batch_frames, np.array([0])   #normal
         else:
             yield batch_frames, np.array([1])   #abnormal
 
+        #batch_frames_flip
         if 'label_A' in train_fn[index]:
             yield batch_frames_flip, np.array([0])  #normal
         else:
@@ -174,8 +186,8 @@ def input_test_video_data(file_name, batch_no=0):
     #file_name = '/raid/DATASETS/anomaly/UCF_Crimes/Videos/Training_Normal_Videos_Anomaly/Normal_Videos308_x264.mp4'
     video = cv2.VideoCapture(file_name)
     total_frame = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = video.get(cv2.CAP_PROP_FPS)
     #mtcnn_detector = mtcnn.mtcnn.MTCNN()
-    #print(file_name + '  ' + str(total_frame))
     divid_no = 1
 
     if total_frame > frame_max:
@@ -186,6 +198,7 @@ def input_test_video_data(file_name, batch_no=0):
             divid_no = int(total_frame / frame_max) + 1
 
     passby = 0
+    #updates the start frame according to the 4000 * batch_no atual
     if batch_no != divid_no - 1:
         while video.isOpened and passby < frame_max * batch_no:
             passby += 1
@@ -200,20 +213,17 @@ def input_test_video_data(file_name, batch_no=0):
                 break
             
     batch_frames = []
-    
     counter = 0
-
-
+    
     while video.isOpened:               
         success, image = video.read()
         if success == False:
             break
-            
+        
         image = cv2.resize(image, (target_width, target_height))
-        
-        image_array = np.array(image)/255.0
-        
+        image_array = np.array(image)/255.0 #normalize
         batch_frames.append(image_array)
+        cv2.imshow('frame', image)
         
         counter += 1
         if counter > frame_max:
@@ -221,9 +231,11 @@ def input_test_video_data(file_name, batch_no=0):
             
     video.release()
     batch_frames = np.array(batch_frames)
-    #print(batch_frames.shape)
-        
-    return np.expand_dims(batch_frames,0), divid_no, total_frame
+    
+    print("\t-batch",batch_no,"[",passby,", ... ] ",batch_frames.shape)    
+    
+    return np.expand_dims(batch_frames,0), divid_no, total_frame, fps
+
 
 def test_files():
     """
@@ -272,20 +284,22 @@ def loss_category(y_true, y_pred):
     return cce
 
 
-def find_model(): 
+def find_weights(): 
     weights_fn = []
     weights_path = []
     weights_base_path = "/media/jtstudents/HDD/.zuble/vigia/zhen_helperino/parameters_saved"
 
     for file in os.listdir(weights_base_path):
-        if file.endswith(".h5"):
+        fname, fext = os.path.splitext(file)
+        if fext == ".h5" and file.find('_2_4_8_xdviolence_model_weights') != -1 :
+            print(file)
             weights_path.append(os.path.join(weights_base_path, file))
             weights_fn.append(file)
 
     return weights_fn, weights_path
 
 def form_model():
-    print("\nFORM_MODEL")
+    print("\nFORM_MODEL\n")
     image_input = keras.Input(shape=(None, target_height, target_width, 3))
     #Freeze the batch normalization
     
@@ -303,6 +317,8 @@ def form_model():
     lstm1 = keras.layers.LSTM(1024,input_shape=(1200,feature_conv_4.shape[2]), return_sequences=True)(feature_conv_4)
     lstm2 = keras.layers.LSTM(512, return_sequences=True)(lstm1)
     global_feature = keras.layers.GlobalMaxPooling1D()(lstm1)
+    
+    #ADD THE AUDIO FEATURE HERE 
     
     dense_1 = keras.layers.Dense(128, activation='relu')(global_feature)
     #dense_2 = keras.layers.Dense(13, activation='sigmoid')(dense_1)
@@ -327,8 +343,6 @@ def train_model():
     MODEL TRAIN/VALIDATION 
     (silent mode - verbose = 0)
     '''
-    physical_devices = tf.config.list_physical_devices('GPU')
-    print("\nAvaiable GPU's",physical_devices)
 
     #https://keras.io/api/callbacks/model_checkpoint/
     ckpt_path = '/media/jtstudents/HDD/.zuble/zhen_helperino/'+time_str+'_2_4_8_xdviolence_anomaly_{epoch:08d}.h5'
@@ -355,39 +369,103 @@ def train_model():
     return model
 
 def test_model(model):
+    print("\nTEST MODEL\n")
     #f = open('/home/zhen/anomaly/test/'+ para_file_name + '.txt', 'w')
-    f = open('/media/jtstudents/HDD/.zuble/zhen_helperino' + time_str + '.txt', 'w')
+    f = open('/media/jtstudents/HDD/.zuble/vigia/zhen_helperino/' + model_weight_fn + '.txt', 'w')
     content_str = ''
-    start_time = time.time()
     total_frames_test = 0
+    predict_total= [] 
+    
+    start_test = time.time()
     for i in range(len(test_fn)):
         if test_fn[i] != '':
             file_path = test_fn[i]
-                            
-            frame1, divid_no, total_frames = input_test_video_data(file_path)
-            total_frames_test += total_frames
-            #print(file_path)
-            predict_result = model.predict(frame1)[0][0]
-            #pred_index = np.argmax(predict_result)
             
+            #the frist 4000 frames from actual test video                
+            frame1, divid_no, total_frames, fps = input_test_video_data(file_path)
+            video_time = total_frames/fps
+            total_frames_test += total_frames
+            
+            start_predict1 = time.time()
+            predict_result = model.predict(frame1)[0][0]
+            end_predict1 = time.time()
+            time_predict = end_predict1-start_predict1
+            
+            high_score_patch = 0
+            print("\t ",predict_result,"%") 
+            
+            
+            #when frame1 (input video) has > 4000 frames
             patch_num = 1
-            while patch_num < divid_no:                           
-                frame1, divid_no, total_frames = input_test_video_data(file_path, patch_num)
+            while patch_num < divid_no:
+                frame1, divid_no, total_frames, fps = input_test_video_data(file_path, patch_num)
+                
+                start_predict2 = time.time()
                 predict_new = model.predict(frame1)[0][0]
+                end_predict2 = time.time()
+                time_predict += end_predict2 - start_predict2
+                
                 if predict_new > predict_result:
                     predict_result = predict_new
+                    high_score_patch = patch_num
+                    
+                print("\t ",predict_result,"%")  
                 patch_num += 1
+                
+            predict_total.append(predict_result)
+            
+            
             if 'label_A' in test_fn[i]:
-                print(test_fn[i][test_fn[i].rindex('/')+1:] + ' ' + str(predict_result) + ' ' + str(i))
+                print('\nNORM',str(i),':',f'{total_frames:.0f}',"@",f'{fps:.0f}',"fps =",f'{video_time:.2f}',"sec\n\t",
+                        test_fn[i][test_fn[i].rindex('/')+1:],
+                        "\n\t "+str(predict_result),"% @batch",high_score_patch,"in",str(time_predict),"seconds\n",
+                        "----------------------------------------------------\n")
             else:
-                print(test_fn[i][test_fn[i].rindex('/')+1:] + ' ' + str(predict_result) + ' ' + str(i))
+                print('\nABNORM',str(i),':',f'{total_frames:.0f}',"@",f'{fps:.0f}',"fps =",f'{video_time:.2f}',"sec\n\t",
+                        test_fn[i][test_fn[i].rindex('/')+1:],
+                        "\n\t"+str(predict_result),"% @batch",high_score_patch,"in",str(time_predict),"seconds\n",
+                        "----------------------------------------------------\n")
+                
             content_str += test_fn[i][test_fn[i].rindex('/')+1:] + '\t' + str(predict_result)  + '\n'
     
-    end_time = time.time()
+    end_test = time.time()
+    time_test = end_test - start_test
 
     f.write(content_str)
     f.close()
-    print(str(total_frames_test / (end_time - start_time)) + 'done...........')                  
+    print("\nDONE\n\ttotal of",str(total_frames_test),"frames processed in",time_test," seconds",
+            "\n\t"+str(total_frames_test / time_test),"frames per second",
+            "\n\n",print(predict_total))                  
+
+'''
+    GPU CONFIGURATION
+    https://www.tensorflow.org/guide/gpu
+'''
+set_tf_loglevel(logging.WARNING)
+
+#Enabling device placement logging causes any Tensor allocations or operations to be printed.
+tf.debugging.set_log_device_placement(False) 
+
+#os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+gpus = tf.config.list_physical_devices('GPU')
+
+
+#https://www.tensorflow.org/api_docs/python/tf/config/experimental/set_memory_growth
+
+#if gpus:
+#    print("\nAvaiable GPU's",gpus)
+#    try:
+#        # Currently, memory growth needs to be the same across GPUs
+#        for gpu in gpus:
+#            tf.config.experimental.set_memory_growth(gpu, True)
+#        
+#        logical_gpus = tf.config.list_logical_devices('GPU')
+#        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#    except RuntimeError as e:
+#        # Memory growth must be set before GPUs have been initialized
+#        print(e)
+
+
 
 
 target_height = 120
@@ -402,15 +480,18 @@ time_str = str(time.time())
 print("\ntime_str=",time_str,"\n")        
 
 
-model = form_model()
+#model = form_model()
 #model = train_model()
 #test_model(model) 
 
 
-weights_fn, weights_path = find_model()
+model = form_model()
+weights_fn, weights_path = find_weights()
+
 for i in range(len(weights_fn)):
-    print("\nLoading weights from",weights_fn[i])
+    print("\n\nLoading weights from",weights_fn[i],"\n")
     model.load_weights(weights_path[i])
+    model_weight_fn,model_weight_ext = os.path.splitext(str(weights_fn[i]))
     test_model(model)
 
 
